6d3b8fb98224669ec614282b06a871da8ff21c86
Violet Chaffee <26vchaffee@castilleja.org>
2024-01-10 17:15:32 -0800

    dealt with some of the todos and got rid of pose object

--- src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -34,26 +34,28 @@ public class DrivetrainSubsystem {
    private static final double SWERVE_TICKS_PER_METER = SWERVE_TICKS_PER_INCH/Constants.METERS_PER_INCH;
 
   /*
-   The maximum voltage that will be delivered to the motors.
-   This can be reduced to cap the robot's maximum speed. Typically, this is useful during initial testing of the robot.
+   * The maximum voltage that will be delivered to the motors.
+   * This can be reduced to cap the robot's maximum speed. Typically, this is useful during initial testing of the robot.
    */
   private static final double MAX_VOLTAGE = 12.0; //TODO: double check this; previously 16.3
-  /*  The formula for calculating the theoretical maximum velocity is:
-     <Motor free speed RPM> / 60 * <Drive reduction> * <Wheel diameter meters> * pi
-     By default this value is setup for a Mk3 standard module using Falcon500s to drive.
-    An example of this constant for a Mk4 L2 module with NEOs to drive is:
-     5880.0 / 60.0 / SdsModuleConfigurations.MK4_L2.getDriveReduction() * SdsModuleConfigurations.MK4_L2.getWheelDiameter() * Math.PI
-   The maximum velocity of the robot in meters per second.
-   This is a measure of how fast the robot should be able to drive in a straight line.
+
+  /* The formula for calculating the theoretical maximum velocity is:
+   * <Motor free speed RPM> / 60 * <Drive reduction> * <Wheel diameter meters> * pi
+   * By default this value is setup for a Mk3 standard module using Falcon500s to drive.
+   * An example of this constant for a Mk4 L2 module with NEOs to drive is:
+   * 5880.0 / 60.0 / SdsModuleConfigurations.MK4_L2.getDriveReduction() * SdsModuleConfigurations.MK4_L2.getWheelDiameter() * Math.PI
+   * The maximum velocity of the robot in meters per second.
+   * This is a measure of how fast the robot should be able to drive in a straight line.
    */
   private static final double MAX_VELOCITY_METERS_PER_SECOND = 6380.0 / 60.0 * // <-- these are copied from SDS library //TODO: check motor free speed RPM
           SdsModuleConfigurations.MK4_L2.getDriveReduction() *
           SdsModuleConfigurations.MK4_L2.getWheelDiameter() * Math.PI;
           // = 5.38281261
-  /**
-  The maximum angular velocity of the robot in radians per second.
-  This is a measure of how fast the robot can rotate in place.
-  Here we calculate the theoretical maximum angular velocity. You can also replace this with a measured amount.
+  
+ /*
+  * The maximum angular velocity of the robot in radians per second.
+  * This is a measure of how fast the robot can rotate in place.
+  * Here we calculate the theoretical maximum angular velocity. You can also replace this with a measured amount.
   */
   private static final double MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND = MAX_VELOCITY_METERS_PER_SECOND /
         Math.hypot(Constants.DRIVETRAIN_TRACKWIDTH_METERS / 2.0, Constants.DRIVETRAIN_WHEELBASE_METERS / 2.0);
@@ -71,12 +73,11 @@ public class DrivetrainSubsystem {
   private SwerveModule backLeftModule;
   private SwerveModule backRightModule;
 
-  private SwerveDrivePoseEstimator odometry; //TODO: explain what this is and how we use it
-  private Pose2d pose; //TODO: explain what this is and how we use it //TODO: refactor code to replace this
+  private SwerveDrivePoseEstimator positionManager;
   private ShuffleboardTab tab;
 
   //ChassisSpeeds takes in y velocity, x velocity, speed of rotation
-  private ChassisSpeeds chassisSpeeds; //TODO: explain what this is and how we use it
+  private ChassisSpeeds chassisSpeeds; //sets expected chassis speed to be called the next time drive is run
 
   public DrivetrainSubsystem() {
         pigeon = new PigeonIMU(Constants.DRIVETRAIN_PIGEON_ID);
@@ -153,11 +154,9 @@ public class DrivetrainSubsystem {
                 new Translation2d(-DRIVETRAIN_WHEELBASE_METERS / 2.0, -DRIVETRAIN_TRACKWIDTH_METERS / 2.0)
         );
 
-        pose = new Pose2d(); //TODO: why does this differ from the pose we feed into odometry?
-
         chassisSpeeds = new ChassisSpeeds(0.0, 0.0, 0.0);
     
-        odometry = new SwerveDrivePoseEstimator(
+        positionManager = new SwerveDrivePoseEstimator(
                 kinematics, 
                 getGyroscopeRotation(), 
                 new SwerveModulePosition[] {
@@ -177,18 +176,18 @@ public class DrivetrainSubsystem {
   }
   //from odometry used for field-relative rotation
   public Rotation2d getPoseRotation() {
-        return pose.getRotation(); 
+        return getPose().getRotation(); 
   }
 
-  public void resetOdometry(Pose2d start){ //TODO: look over this method and confirm use
+  //log the current position within the positionManager so that it knows what our encoders say about our position
+  //the positionManager will then update its pose estimate, accounting for any drift
+  public void updatePositionManager(){
         SwerveModulePosition[] positionArray =  new SwerveModulePosition[] {
                 new SwerveModulePosition(frontLeftModule.getPosition()/SWERVE_TICKS_PER_METER, new Rotation2d(frontLeftModule.getSteerAngle())),
                 new SwerveModulePosition(frontRightModule.getPosition()/SWERVE_TICKS_PER_METER, new Rotation2d(frontRightModule.getSteerAngle())), 
                 new SwerveModulePosition(backLeftModule.getPosition()/SWERVE_TICKS_PER_METER, new Rotation2d(backLeftModule.getSteerAngle())),
                 new SwerveModulePosition(backRightModule.getPosition()/SWERVE_TICKS_PER_METER, new Rotation2d(backRightModule.getSteerAngle()))};
-        pose = start; //TODO: taken from elsewhere, confirm why we do this
-        odometry.resetPosition(getGyroscopeRotation(), positionArray, pose);
-        pose = odometry.update(getGyroscopeRotation(), positionArray);
+        positionManager.update(getGyroscopeRotation(), positionArray);
 }
        
 //the next iteration of drive will use this speed  
@@ -225,7 +224,7 @@ public void setSpeed(ChassisSpeeds chassisSpeed) {
                 new SwerveModulePosition(backLeftModule.getPosition()/SWERVE_TICKS_PER_METER, new Rotation2d(backLeftModule.getSteerAngle())),
                 new SwerveModulePosition(backRightModule.getPosition()/SWERVE_TICKS_PER_METER, new Rotation2d(backRightModule.getSteerAngle()))
         };
-        pose = odometry.update(getGyroscopeRotation(), modulePositions); 
+        positionManager.update(getGyroscopeRotation(), modulePositions); 
 
         //array of states filled with the speed and angle for each module (made from linear and angular motion for the whole robot) 
         SwerveModuleState[] states = kinematics.toSwerveModuleStates(chassisSpeeds);
@@ -262,20 +261,24 @@ public void setSpeed(ChassisSpeeds chassisSpeed) {
 
     //AUTO AND FAILSAFE
     public void stopDrive() {
-        //TODO: always log something very explicit and scary here
+        System.out.println("STOPPED DRIVE SOMETHING MIGHT HAVE GONE WRONG");
         setSpeed(ChassisSpeeds.fromFieldRelativeSpeeds(0.0, 0.0, 0.0, getPoseRotation()));
         drive();
    }
 
     public double getPoseX(){
-        return pose.getX();
+        return getPose().getX();
     }
 
     public double getPoseY(){
-        return pose.getY();
+        return getPose().getY();
     }
 
     public double getPoseDegrees(){
-        return pose.getRotation().getDegrees();
+        return getPose().getRotation().getDegrees();
+    }
+ 
+    private Pose2d getPose(){
+        return positionManager.getEstimatedPosition();
     }
 }
