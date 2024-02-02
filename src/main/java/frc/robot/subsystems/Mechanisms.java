1359e666f395aad267480c5916e20779c4bfe9b5
karyschang <26kchang@castilleja.org>
2024-02-01 19:48:23 -0800

    made changes to Mechanisms.java

--- src/main/java/frc/robot/subsystems/Mechanisms.java
+++ src/main/java/frc/robot/subsystems/Mechanisms.java
@@ -37,7 +37,7 @@ public class Mechanisms {
         sensorSubsystem.init();
         elevatorSubsystem.init();
 
-        mechanismState = MechanismStates.HOLDING; //TODO: figure out what state to start in
+        setState(MechanismStates.HOLDING); //TODO: figure out what state to start in
     }
 
     public void periodic(){
@@ -46,7 +46,7 @@ public class Mechanisms {
             shooterSubsystem.setState(ShooterSubsystem.ShooterStates.AMP); //default shooter on
             elevatorSubsystem.setState(ElevatorSubsystem.ElevatorStates.LOW_HEIGHT);
             if (sensorSubsystem.detectNote()){
-                mechanismState = MechanismStates.HOLDING;
+                setState(MechanismStates.HOLDING);
             }
         } else if(mechanismState == MechanismStates.HOLDING){
             intakeSubsystem.setState(IntakeSubsystem.IntakeStates.OFF);
@@ -59,9 +59,8 @@ public class Mechanisms {
                 shooterSubsystem.setState(ShooterSubsystem.ShooterStates.AMP);
                 elevatorSubsystem.setState(ElevatorSubsystem.ElevatorStates.AMP_HEIGHT);
             }
-            if(System.currentTimeMillis()-stateStartTime >= 2000){ // 5 secs should be too long for shooting but just in case
-                mechanismState = MechanismStates.OFF;
-                isFirstTimeInState = true; //why back to true
+            if(System.currentTimeMillis()-stateStartTime >= 5000){ // 5 secs should be too long for shooting but just in case
+                setState(MechanismStates.OFF);
             }
         } else if(mechanismState == MechanismStates.SHOOTING_SPEAKER){
             if (isFirstTimeInState){
@@ -70,9 +69,8 @@ public class Mechanisms {
                 shooterSubsystem.setState(ShooterSubsystem.ShooterStates.SPEAKER);
                 elevatorSubsystem.setState(ElevatorSubsystem.ElevatorStates.LOW_HEIGHT);
             }
-            if(System.currentTimeMillis()-stateStartTime >= 2000){
-                mechanismState = MechanismStates.OFF;
-                isFirstTimeInState = true;
+            if(System.currentTimeMillis()-stateStartTime >= 5000){ // 5 secs should be too long for shooting but just in case
+                setState(MechanismStates.OFF);
             }
         } else if(mechanismState == MechanismStates.LOW_ELEVATOR_HEIGHT){ // TODO: figure out if we need this bc setting it in SHOOTING_SPEAKER might be enough
             elevatorSubsystem.setState(ElevatorSubsystem.ElevatorStates.LOW_HEIGHT);
@@ -81,11 +79,11 @@ public class Mechanisms {
         } else if (mechanismState == MechanismStates.OFF){
             shooterSubsystem.setState(ShooterSubsystem.ShooterStates.OFF);
             intakeSubsystem.setState(IntakeSubsystem.IntakeStates.OFF);
-            elevatorSubsystem.setState(ElevatorSubsystem.ElevatorStates.ZERO);
+            elevatorSubsystem.setState(ElevatorSubsystem.ElevatorStates.STOPPED);
         } else {
             shooterSubsystem.setState(ShooterSubsystem.ShooterStates.OFF);
             intakeSubsystem.setState(IntakeSubsystem.IntakeStates.OFF);
-            elevatorSubsystem.setState(ElevatorSubsystem.ElevatorStates.ZERO);
+            elevatorSubsystem.setState(ElevatorSubsystem.ElevatorStates.STOPPED);
             System.out.println("WHAT ARE YOU DOING----STATE NOT RECOGNIZED!!!!!");
             System.out.println("CURRENT STATE: " + mechanismState);
         }
@@ -96,5 +94,4 @@ public class Mechanisms {
         stateStartTime = System.currentTimeMillis(); //first time we set stateStartTime. what's this for? should this be in init? mayhaps
         isFirstTimeInState = true;
     }
-
 }
