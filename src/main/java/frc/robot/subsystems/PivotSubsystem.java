98a34a8ddd4c359725aeb4e6fba9bc35a92d0148
AnaikaW <25awalia@castilleja.org>
2024-02-23 10:56:43 -0800

    fixed pivotsub

--- src/main/java/frc/robot/subsystems/PivotSubsystem.java
+++ src/main/java/frc/robot/subsystems/PivotSubsystem.java
@@ -14,12 +14,15 @@ public class PivotSubsystem{
     public DigitalInput bottomLimitSwitch;
 
 
-    private final double PIVOT_SPEED = 0.1;
+    private final double PIVOT_SPEED = 0.08;
+    private final double MANUAL_SPEED = 0.06;
     
 
     public static enum PivotStates{
         SPEAKER,
         AMP,
+        MANUAL_UP,
+        MANUAL_DOWN,
         OFF;
     }
 
@@ -30,33 +33,37 @@ public class PivotSubsystem{
         pivot = new TalonFX(Constants.PIVOT_MOTOR_CAN_ID);
         topLimitSwitch = new DigitalInput(0); //check these ports
         bottomLimitSwitch = new DigitalInput(7); 
+        
+        init();
     }
     
     public void init(){
-        pivotState = PivotStates.AMP;
+        pivotState = PivotStates.OFF;
     }
 
     public void periodic(){//limit switches true, then false when pressed
         System.out.println("CURRENT PIVOT STATE: " + pivotState);
+        System.out.println("top limit switch: " + topLimitSwitch.get());
+        System.out.println("bottom limit switch: " + bottomLimitSwitch.get());
         if((pivotState == PivotStates.SPEAKER) && topLimitSwitch.get()){
-            System.out.println("in periodic 1");
             pivot.set(ControlMode.PercentOutput, PIVOT_SPEED);
         }else if((pivotState == PivotStates.AMP) && bottomLimitSwitch.get()){
             pivot.set(ControlMode.PercentOutput, -PIVOT_SPEED);
-            System.out.println("in periodic 2");
+        }else if((pivotState == PivotStates.MANUAL_UP) && topLimitSwitch.get()){
+            pivot.set(ControlMode.PercentOutput, MANUAL_SPEED);
+    
+        }else if((pivotState == PivotStates.MANUAL_DOWN) && bottomLimitSwitch.get()){
+            pivot.set(ControlMode.PercentOutput, -MANUAL_SPEED);
         }else{
-            if(pivotState == PivotStates.AMP && !bottomLimitSwitch.get()){
+            //these if statements are for the "windshield wiper" motion (back and forth)
+            /*if(pivotState == PivotStates.AMP && !bottomLimitSwitch.get()){
                 pivotState = PivotStates.SPEAKER;
             }
-            
             if(pivotState == PivotStates.SPEAKER && !topLimitSwitch.get()){
                 pivotState = PivotStates.AMP;
-            }
-            //pivotState = PivotStates.OFF;
-            //pivot.set(ControlMode.PercentOutput, 0);
-            System.out.println("in periodic 3");
-            System.out.println("top limit switch: " + topLimitSwitch.get());
-            System.out.println("bottom limit switch: " + bottomLimitSwitch.get());
+            }*/
+            pivotState = PivotStates.OFF;
+            pivot.set(ControlMode.PercentOutput, 0);
         }
     }
    
