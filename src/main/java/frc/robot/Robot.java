8741b17e3c5eda8680c897dc1d90d19435d5fe20
Anaika Walia <anaikawalia@Anaikas-Laptop.local>
2024-10-23 13:50:38 -0700

    a

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -2,55 +2,53 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.subsystems.ServoSubsystem;
-import frc.robot.commands.MoveServoToAngleCommand;
+import frc.robot.subsystems.LimelightSubsystem;
 
 public class Robot extends TimedRobot {
 
-    private ServoSubsystem servoSubsystem;
-    private MoveServoToAngleCommand moveServoToAngleCommand;
+    private LimelightSubsystem limelightSubsystem;
+
 
     @Override
     public void robotInit() {
-        // Initialize the ServoSubsystem
-        servoSubsystem = new ServoSubsystem(0); // Servo on PWM port 1
+
+        limelightSubsystem = new LimelightSubsystem();
+
     }
 
     @Override
     public void robotPeriodic() {
-        // Run the CommandScheduler periodically
         CommandScheduler.getInstance().run();
     }
 
     @Override
     public void teleopInit() {
-        // When teleop starts, move the servo to a hardcoded angle (e.g., 90 degrees)
-        moveServoToAngleCommand = new MoveServoToAngleCommand(servoSubsystem, 90); // Rotate to 90 degrees
-        moveServoToAngleCommand.schedule(); // Schedule the command to run immediately
+      
     }
 
     @Override
     public void teleopPeriodic() {
-        // Called periodically during teleop
+        double tyValue = limelightSubsystem.getVerticalOffset();
+        System.out.println("Limelight ty (vertical offset): " + tyValue);
     }
 
     @Override
     public void autonomousInit() {
-        // Add autonomous commands here if needed
+      
     }
 
     @Override
     public void autonomousPeriodic() {
-        // Called periodically during autonomous
+        
     }
 
     @Override
     public void disabledInit() {
-        // Called once when the robot is disabled
+        
     }
 
     @Override
     public void disabledPeriodic() {
-        // Called periodically while disabled
+       
     }
 }
