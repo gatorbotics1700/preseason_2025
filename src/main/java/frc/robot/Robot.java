9e1797d80b27ae7a6dedfd49a0691571c660caf1
Anaika Walia <anaikawalia@Anaikas-Laptop.local>
2024-11-21 19:17:38 -0800

    A

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -3,20 +3,13 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.subsystems.DrivetrainSubsystem;
 
 public class Robot extends TimedRobot {
-    @SuppressWarnings("unused")
-
     private Command m_autonomousCommand;
-    private Command m_testCommand;
     private RobotContainer container;
-    private Command m_teleCommand;
-    private DrivetrainSubsystem m_drivetrainSubsystem;
 
     @Override
-    
-    public void robotInit(){
+    public void robotInit() {
         container = new RobotContainer();
     }
 
@@ -24,33 +17,26 @@ public class Robot extends TimedRobot {
     public void robotPeriodic() {
         CommandScheduler.getInstance().run();
     }
+
     @Override
-    public void autonomousInit(){
-        // m_testCommand = container.getAutonomousCommand();
-
-        // if(m_testCommand != null){
-        //     m_testCommand.schedule();
-        // }
-
+    public void autonomousInit() {
         m_autonomousCommand = container.getAutonomousCommand();
 
-        if(m_autonomousCommand != null){
+        if (m_autonomousCommand != null) {
             m_autonomousCommand.schedule();
         }
     }
+
     @Override
     public void teleopInit() {
         // This makes sure that the autonomous stops running when teleop starts
         if (m_autonomousCommand != null) {
             m_autonomousCommand.cancel();
         }
-        
-        // Remove the teleop command scheduling since we're using default command
-        m_drivetrainSubsystem.setDefaultCommand(container.getDrivetrain().getDefaultCommand());
     }
 
     @Override
     public void teleopPeriodic() {
-        // Remove this entire method or leave it empty
+        // Leave empty - default command will handle teleop
     }
 }
