4c904d52500bfb8b6183c116fabc0aa9c87619c9
Anaika Walia <anaikawalia@Anaikas-Laptop.local>
2024-12-09 16:49:35 -0800

    a

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,10 +4,12 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
+import edu.wpi.first.wpilibj2.command.PrintCommand;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.commands.LimelightControlCommand;
 import frc.robot.commands.ServoControlCommand;
 import frc.robot.commands.TurretControlCommand;
@@ -15,43 +17,42 @@ import frc.robot.subsystems.LimelightSubsystem;
 import frc.robot.subsystems.ServoSubsystem;
 import frc.robot.subsystems.TurretSubsystem;
 
+
 public class RobotContainer {
-    public static final CommandXboxController m_controller_two = new CommandXboxController(1);
-    private static final TurretSubsystem m_turretsub = new TurretSubsystem();
-    private static final LimelightSubsystem m_limelightsub = new LimelightSubsystem();
-    private static final ServoSubsystem m_servosub = new ServoSubsystem();
+  // The robot's subsystems and commands are defined here...
+  public static final Joystick m_joystick = new Joystick(0);
+  private static final TurretSubsystem m_turretsub = new TurretSubsystem();
+  private static final LimelightSubsystem m_limelightsub = new LimelightSubsystem();
+  private static final ServoSubsystem m_servosub = new ServoSubsystem();
 
-    private int currentTargetID = 1; // Initialize with the first target ID
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public RobotContainer() {
+    // Configure the button bindings
+    // m_drive.setDefaultCommand(new DrivewithJoysticks(m_drive, m_joystick.getRawAxis(0), m_joystick.getRawAxis(1)));
+    
+    // SendableRegistry.add(m_drive, "drive");
+    // configureButtonBindings();
+  }
 
-    /** The container for the robot. Contains subsystems, OI devices, and commands. */
-    public RobotContainer() {
-        configureButtonBindings();
-    }
+  private void configureButtonBindings() {
+    JoystickButton slower = new JoystickButton(m_joystick, 1);
+    JoystickButton faster = new JoystickButton(m_joystick, 2);
 
-    private void configureButtonBindings() {
-        Trigger aButton = m_controller_two.a();
-        Trigger bButton = m_controller_two.b();
 
-        aButton.onTrue(() -> {
-            currentTargetID = 9; // Set target ID to 9 when A is pressed
-            System.out.println("Current Target ID: " + currentTargetID);
-        });
+    // slower.whileHeld(new SpinSlower(m_spinner));
+    // faster.whileHeld(new SpinFaster(m_spinner));
 
-        bButton.onTrue(() -> {
-            currentTargetID = 8; // Reset target ID to 0 when B is pressed
-            System.out.println("Current Target ID: " + currentTargetID);
-        });
-    }
+  }
 
-    public Command getLimelightCommand() {
-        return new LimelightControlCommand(m_limelightsub, m_turretsub, 9, 1); // Pass the specific target ID and pipeline ID
-    }
+  public Command getLimelightCommand() {
+    return new LimelightControlCommand(m_limelightsub, m_turretsub, 8);
+  }
 
-    public Command getServoCommand() {
-        return new ServoControlCommand(m_servosub, 50);
-    }
+  public Command getServoCommand() {
+    return new ServoControlCommand(m_servosub, 50);
+  }
 
-    public Command getTurretCommand() {
-        return new TurretControlCommand(m_turretsub, m_limelightsub, 0.05);
-    }
+  public Command getTurretCommand() {
+    return new TurretControlCommand(m_turretsub, m_limelightsub, 0.05);
+  }
 }
