bcfc9840ca372b153aaea2a368fc8eb3ea28d78a
Gatorbotics <gatorbotics1700@gmail.com>
2024-12-12 19:50:48 -0800

    we changed gyroscope -AK, PC, AR

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,7 +9,7 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 public class RobotContainer {
@@ -23,7 +23,7 @@ public class RobotContainer {
 
         // Zero gyroscope button binding
         new Trigger(controller::getBackButtonPressed)
-                .onTrue(new RunCommand(drivetrain::zeroGyroscope));
+                .onTrue(new InstantCommand(drivetrain::zeroGyroscope));
 
         // Auto chooser setup
         autoChooser = AutoBuilder.buildAutoChooser();
