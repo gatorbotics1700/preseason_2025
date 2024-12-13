803e40a0f736480b661aec487aa8cb05d32ea9dd
Gatorbotics <gatorbotics1700@gmail.com>
2024-12-12 18:20:28 -0800

    set offsets for dory drivetrain - anne + phoenix

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -17,11 +17,19 @@ public class Constants {
     // public static final MechanicalConfiguration MODULE_CONFIGURATION = SdsModuleConfigurations.MK4_L2;
 
      //nemo
-    public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(336.094);
+    /* public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(336.094);
     public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(225.176);
     public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(243.369);
     public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(204.256);
     public static final MechanicalConfiguration MODULE_CONFIGURATION = SdsModuleConfigurations.MK4I_L2;
+*/
+
+//dory
+    public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(59.414);
+    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(194.941 + 180);
+    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(7.207);
+    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(242.574);
+    public static final MechanicalConfiguration MODULE_CONFIGURATION = SdsModuleConfigurations.MK4I_L2; 
 
 
     public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 21;
