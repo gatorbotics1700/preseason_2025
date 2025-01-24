2029d4a958050067d3ff34dc0b1a78987f04643b
Gatorbotics <gatorbotics1700@gmail.com>
2025-01-23 16:24:06 -0800

    worked on elevator command

--- src/main/java/frc/robot/commands/ElevatorCommand.java
+++ src/main/java/frc/robot/commands/ElevatorCommand.java
@@ -1,7 +1,27 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.ElevatorSubsystem;
 
 public class ElevatorCommand extends Command {
+
+    private ElevatorSubsystem elevatorSubsystem;
+    private double position;
+    public ElevatorCommand(ElevatorSubsystem elevatorSubsystem, double position){
+        this.elevatorSubsystem = elevatorSubsystem;
+        this.position = position;
+        addRequirements(elevatorSubsystem);
+    }
     
+    @Override 
+    public void execute(){
+        elevatorSubsystem.setPosition(position);
+    }
+
+    @Override
+    public boolean isFinished() {
+        // TODO Auto-generated method stub
+        return super.isFinished();
+    }
+
 }
