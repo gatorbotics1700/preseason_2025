96b080f00bf96aefe0720d38df24b663279688b7
Gatorbotics <gatorbotics1700@gmail.com>
2025-01-28 17:07:23 -0800

    wrote the pid for elevator grah

--- src/main/java/frc/robot/commands/ElevatorCommand.java
+++ src/main/java/frc/robot/commands/ElevatorCommand.java
@@ -1,31 +1,33 @@
-// package frc.robot.commands;
+package frc.robot.commands;
 
-// import edu.wpi.first.wpilibj2.command.Command;
-// import frc.robot.subsystems.ElevatorSubsystem;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.ElevatorSubsystem;
 
-// public class ElevatorCommand extends Command {
+public class ElevatorCommand extends Command {
 
-//     private ElevatorSubsystem elevatorSubsystem;
-//     private double height; // in inches
-//     private double DEADBAND = 5000; // TODO: change this value
+    private ElevatorSubsystem elevatorSubsystem;
+    private double height; // in inches
+    double desiredTicks;
+    private double DEADBAND = 5000; // TODO: change this value
     
-//     public ElevatorCommand(ElevatorSubsystem elevatorSubsystem, double height){
-//         this.elevatorSubsystem = elevatorSubsystem;
-//         this.height = height;
-//         addRequirements(elevatorSubsystem);
-//     }
+    public ElevatorCommand(ElevatorSubsystem elevatorSubsystem, double height){
+        this.elevatorSubsystem = elevatorSubsystem;
+        this.height = height;
+        desiredTicks = elevatorSubsystem.determineInchesToTicks(height);
+        addRequirements(elevatorSubsystem);
+    }
     
-//     @Override 
-//     public void execute(){
-//         elevatorSubsystem.setPosition(elevatorSubsystem.determineInchesToTicks(height));
-//     }
+    @Override 
+    public void execute(){
+        elevatorSubsystem.setPosition(desiredTicks);
+    }
 
-//     @Override
-//     public boolean isFinished() {
-//         if(Math.abs(elevatorSubsystem.getPosition() - height) < DEADBAND){
-//             return true;
-//         }
-//         return false;
-//     }
-
-// }
+    @Override
+    public boolean isFinished() {
+        double error = desiredTicks - elevatorSubsystem.getPosition();
+        if(Math.abs(error) < DEADBAND){
+            return true;
+        }
+        return false;
+    }
+}
--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -4,6 +4,7 @@ import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
@@ -23,20 +24,33 @@ public class ElevatorSubsystem extends SubsystemBase {
 
     private final DigitalInput colorSensor;
 
+    private final PIDController elevatorPidController;
+
     private static final double kP = 0.0; // TODO: change this value
     private static final double kI = 0.0; // TODO: change this value
     private static final double kD = 0.0; // TODO: change this value
+    
     private static final int kIzone = 0; // TODO: change this value
     private static final double kPeakOutput = 0.0; // TODO: change this value
+    
+    private static final int SLOT_IDX = 0;
+    private static final int PID_LOOP_IDX = 0;
+    private static final int CONFIG_TIMEOUT_MS = 0;
+    //add height and stuff
 
     public ElevatorSubsystem(){
         elevatorMotor = new TalonFX(Constants.ELEVATOR_CAN_ID);
         colorSensor = new DigitalInput(0);
+        elevatorPidController = new PIDController(kP, kI, kD);
     }
 
     public void setPosition(double desiredTicks){
-        if(Math.abs(desiredTicks - getPosition()) > DEADBAND){
-            elevatorMotor.setControl(positionVoltage.withPosition(desiredTicks));
+        double currentTicks = getPosition();
+        double error = desiredTicks - currentTicks;
+        if(Math.abs(error) > DEADBAND){
+            double output = elevatorPidController.calculate(currentTicks, desiredTicks);
+            elevatorMotor.setControl(dutyCycleOut.withOutput(output));
+            //elevatorMotor.setControl(positionVoltage.withPosition(desiredTicks));
         }else{
             elevatorMotor.setControl(dutyCycleOut.withOutput(0));
         }
