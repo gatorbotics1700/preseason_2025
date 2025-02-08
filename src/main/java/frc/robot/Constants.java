0fe277560953e0f9390f7bc10705e7b583af579b
gatorbotics <gatorbotics1700@gmail.com>
2025-02-08 14:13:39 -0800

    added climbing subystem and changed climbing motor speeds/CAN ID labels

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -57,9 +57,8 @@ public class Constants {
     public static final int SERVO_PWM_PORT = 7;
     public static final double SERVO_ANGLE = 45.0;
 
-    public static final double CLIMBING_SPEED = -0.1; 
-    public static final int LEFT_CLIMBING_MOTOR_CAN_ID = 0;
-    public static final int RIGHT_CLIMBING_MOTOR_CAN_ID = 20;
+    public static final double CLIMBING_SPEED = -0.15; 
+    public static final int CLIMBING_MOTOR_CAN_ID = 0;
 
     //TODO: find the real values of ALL of these constants
     public static final int ELEVATOR_CAN_ID = 17; //TODO: get actual CAN ID
