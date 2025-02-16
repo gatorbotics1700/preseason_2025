151b7bc94e1c8fae3fb8a8c8457e8652de562e14
Violet Chaffee <26vchaffee@castilleja.org>
2025-02-15 20:34:02 -0800

    renamed things for clarity:

--- src/main/java/frc/robot/commands/ScoreCommands.java
+++ src/main/java/frc/robot/commands/ScoreCommands.java
@@ -1,22 +1,20 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.Constants;
-import frc.robot.RobotContainer;
 import frc.robot.subsystems.CoralShooterSubsystem;
 import frc.robot.subsystems.ElevatorSubsystem;
 import frc.robot.subsystems.StickSubsystem;
 import frc.robot.subsystems.StickPivotSubsystem;
 
 public class ScoreCommands{
-    public static Command elevatorShooter(int level, ElevatorSubsystem m_elevatorSub, StickSubsystem m_stickSub, StickPivotSubsystem m_stickPivotSub){
+    public static Command Level(int level, ElevatorSubsystem m_elevatorSub, StickSubsystem m_stickSub, StickPivotSubsystem m_stickPivotSub){
         System.out.println("IN SCORE COMMAND!!!");
         AutoElevatorScorerCommand autoElevatorScorerCommand = new AutoElevatorScorerCommand(m_elevatorSub, m_stickSub, m_stickPivotSub, level);
         return autoElevatorScorerCommand;
     }
 
-    public static Command coralShooter(CoralShooterSubsystem m_coralShooterSub){
+    public static Command Shoot(CoralShooterSubsystem m_coralShooterSub){
             System.out.println("shooting!");
             CoralShooterCommand coralShooterCommand = new CoralShooterCommand(m_coralShooterSub,  Constants.CORAL_OUTTAKING_SPEED);
             return coralShooterCommand; //TODO: Change speed
