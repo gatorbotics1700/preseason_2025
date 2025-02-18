9d29703187ab592a762e8a62df9a0e6ee2afefa6
gatorbotics <gatorbotics1700@gmail.com>
2025-02-18 12:34:54 -0800

    updated auto paths for hulk

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -15,18 +15,18 @@ public class Constants {
     public static final int DRIVETRAIN_PIGEON_ID = 6;
 
     // hulk
-    // public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(118.6253437499); //116.806640625
-    // public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(283.7109375); //282.919921875
-    // public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(124.0988159179); //121.640624999
-    // public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(38.75976562499); //227.548
-    // public static final MechanicalConfiguration MODULE_CONFIGURATION = SdsModuleConfigurations.MK4_L2;
-    // public static final String CANIVORE_BUS_NAME = "";
-    // public static final double LIMELIGHT_FORWARD_OFFSET = 0.3429;
-    // public static final double LIMELIGHT_SIDE_OFFSET = 0.0;
-    // public static final double LIMELIGHT_UP_OFFSET = 0.269875;
-    // public static final double LIMELIGHT_YAW_OFFSET = 0.0;
-    // public static final double LIMELIGHT_PITCH_OFFSET = -2.0;
-    // public static final double LIMELIGHT_ROLL_OFFSET = -1.0;
+    public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(118.6253437499); //116.806640625
+    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(283.7109375); //282.919921875
+    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(124.0988159179); //121.640624999
+    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(38.75976562499); //227.548
+    public static final MechanicalConfiguration MODULE_CONFIGURATION = SdsModuleConfigurations.MK4_L2;
+    public static final String CANIVORE_BUS_NAME = "";
+    public static final double LIMELIGHT_FORWARD_OFFSET = 0.3429;
+    public static final double LIMELIGHT_SIDE_OFFSET = 0.0;
+    public static final double LIMELIGHT_UP_OFFSET = 0.269875;
+    public static final double LIMELIGHT_YAW_OFFSET = 0.0;
+    public static final double LIMELIGHT_PITCH_OFFSET = -2.0;
+    public static final double LIMELIGHT_ROLL_OFFSET = -1.0;
 
     //nemo
     // public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(336.094);
@@ -43,18 +43,18 @@ public class Constants {
     // public static final double LIMELIGHT_ROLL_OFFSET = 0.0;
 
     // dory
-    public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(98.173828125);
-    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(149.23828125);
-    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(333.10546875);
-    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(102.8320312500);
-    public static final MechanicalConfiguration MODULE_CONFIGURATION = SdsModuleConfigurations.MK4I_L2;
-    public static final String CANIVORE_BUS_NAME = "";
-    public static final double LIMELIGHT_FORWARD_OFFSET = 0.36195;
-    public static final double LIMELIGHT_SIDE_OFFSET = -0.2413;
-    public static final double LIMELIGHT_UP_OFFSET = 0.200025;
-    public static final double LIMELIGHT_YAW_OFFSET = 2.0;
-    public static final double LIMELIGHT_PITCH_OFFSET = 28.0;
-    public static final double LIMELIGHT_ROLL_OFFSET = 0.0;
+    // public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(98.173828125);
+    // public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(149.23828125);
+    // public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(333.10546875);
+    // public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(102.8320312500);
+    // public static final MechanicalConfiguration MODULE_CONFIGURATION = SdsModuleConfigurations.MK4I_L2;
+    // public static final String CANIVORE_BUS_NAME = "";
+    // public static final double LIMELIGHT_FORWARD_OFFSET = 0.36195;
+    // public static final double LIMELIGHT_SIDE_OFFSET = -0.2413;
+    // public static final double LIMELIGHT_UP_OFFSET = 0.200025;
+    // public static final double LIMELIGHT_YAW_OFFSET = 2.0;
+    // public static final double LIMELIGHT_PITCH_OFFSET = 28.0;
+    // public static final double LIMELIGHT_ROLL_OFFSET = 0.0;
 
     public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 21;
     public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 20; 
@@ -78,7 +78,7 @@ public class Constants {
     // for the old coral shooter mechanism
     public static final int CORAL_SHOOTER_CAN_ID = 30;
     public static final double CORAL_INTAKING_SPEED = 0.25;
-    public static final double CORAL_OUTTAKING_SPEED = -0.7; 
+    public static final double CORAL_OUTTAKING_SPEED = -0.75; 
     public static final double SERVO_ANGLE = 45.0;
 
     public static final double CLIMBING_SPEED = -0.15; 
